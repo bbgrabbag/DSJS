7318f1efff782ff9b0788d6acedec4be398a903a
Ben Turner <bbgrabbag@gmail.com>
2020-06-08 12:36:22 -0600

    initial project config and test setup

---
+++ src/linked-list/linked-list.spec.ts
@@ -0,0 +1,9 @@
+import {LinkedList} from '../index'
+
+describe('Linked List', () =>{
+
+    it('Should initialize correctly', () =>{
+        const ll = new LinkedList
+        expect(ll).toBeDefined()
+    })
+})
