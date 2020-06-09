f68930cee2f623f835eac50bb6643605fb1bf630
Ben Turner <bbgrabbag@gmail.com>
2020-06-08 22:29:46 -0600

    singly linked list core completed

--- src/linked-list/index.ts
+++ src/linked-list/index.ts
@@ -1,3 +1,122 @@
-export class LinkedList{
-    
+export class LinkedListNode<D> {
+    private _value: D
+    private _next: LinkedListNode<D> | null = null
+
+    constructor(value: D) {
+        this._value = value;
+    }
+
+    set value(value: D) {
+        this._value = value;
+    }
+
+    get value(): D {
+        return this._value;
+    }
+
+    set next(node: LinkedListNode<D> | null) {
+        if (this.next !== null) {
+            if (node !== null) {
+                const tmp: LinkedListNode<D> = this.next;
+                node.next = tmp;
+            }
+        }
+        this._next = node;
+    }
+
+    get next(): LinkedListNode<D> | null {
+        return this._next;
+    }
+}
+
+export class LinkedList<D> {
+    private _head: LinkedListNode<D> | null = null
+
+    constructor(headValue?: D)
+    constructor(head?: LinkedListNode<D>)
+    constructor(values?: D[])
+    constructor(initializer?: D | D[] | LinkedListNode<D>) {
+        if (initializer === undefined) return;
+        if (Array.isArray(initializer)) {
+            if (!initializer.length) return;
+            this._head = new LinkedListNode<D>(initializer.shift() as D);
+            let next = this._head;
+            initializer.forEach(v => {
+                next.next = new LinkedListNode<D>(v);
+                next = next.next;
+            });
+        } else if (initializer instanceof LinkedListNode) {
+            this._head = initializer;
+        } else {
+            this._head = new LinkedListNode<D>(initializer);
+        }
+    }
+
+    set head(node: LinkedListNode<D> | null) {
+        this._head = node;
+    }
+
+    get head(): LinkedListNode<D> | null {
+        return this._head;
+    }
+
+    prepend(value: D): LinkedListNode<D>
+    prepend(node: LinkedListNode<D>): LinkedListNode<D>
+    prepend(arg: D | LinkedListNode<D>): LinkedListNode<D> {
+        const node = arg instanceof LinkedListNode ? arg : new LinkedListNode<D>(arg);
+        if (this.head === null) return this.head = node;
+        node.next = this.head;
+        return this.head = node;
+    }
+
+    append(value: D): LinkedListNode<D>
+    append(node: LinkedListNode<D>): LinkedListNode<D>
+    append(arg: D | LinkedListNode<D>): LinkedListNode<D> {
+        const node = arg instanceof LinkedListNode ? arg : new LinkedListNode<D>(arg);
+
+        if (this.head === null) return this.head = node;
+
+        let next: LinkedListNode<D> | null = this.head;
+        while (next?.next !== null) {
+            next = this.head.next;
+        }
+        return next.next = node;
+    }
+
+    find(cb: (node: LinkedListNode<D>) => boolean): LinkedListNode<D> | undefined {
+        let next = this.head;
+        while (next !== null) {
+            if (cb(next)) return next;
+            next = next.next;
+        }
+    }
+
+    remove(node: LinkedListNode<D>): void {
+        if (this.head === null) return;
+        if (node === this.head) {
+            this.head = this.head.next;
+            return;
+        }
+
+        let next = this.head.next;
+        let prev = this.head;
+
+        while (next !== null) {
+            if (node === next) {
+                prev.next = next.next;
+                next = null;
+                return;
+            }
+            prev = next;
+            next = next.next;
+        }
+    }
+
+    *[Symbol.iterator](): Generator<D> {
+        let nextNode = this.head;
+        while (nextNode !== null) {
+            yield nextNode.value;
+            nextNode = nextNode.next;
+        }
+    }
 }
--- src/linked-list/linked-list.spec.ts
+++ src/linked-list/linked-list.spec.ts
@@ -1,9 +1,99 @@
-import {LinkedList} from '../index'
+import { LinkedList, LinkedListNode } from '.';
 
-describe('Linked List', () =>{
+describe('Linked list nodes', () => {
+    it('Should initialize with or without a null value', () => {
+        const node = new LinkedListNode<number>(10);
+        expect(node.value).toBe(10);
+        expect(node.next).toBeNull();
+    });
+    it('Should be able to set new node values', () => {
+        const node = new LinkedListNode<number>(10);
+        node.value = 5;
+        expect(node.value).toBe(5);
+    });
+    it('Should insert nodes correctly', () => {
+        const node = new LinkedListNode<number>(1);
+        node.next = new LinkedListNode<number>(3);
+        expect(node.value).toBe(1);
+        expect(node.next.value).toBe(3);
 
-    it('Should initialize correctly', () =>{
-        const ll = new LinkedList
-        expect(ll).toBeDefined()
-    })
-})
+        node.next = new LinkedListNode<number>(2);
+        expect(node.next.value).toBe(2);
+        expect(node.next?.next?.value).toBe(3);
+
+        node.next = null;
+        expect(node.next).toBeNull();
+
+    });
+});
+
+describe('Linked list', () => {
+    it('Should initialize with no value', () => {
+        const ll = new LinkedList<number>();
+        expect(ll.constructor.name).toBe('LinkedList');
+        expect(ll.head).toBeNull();
+    });
+
+    it('Should initialize with single value', () => {
+        const ll = new LinkedList<number>(5);
+        expect(ll.head?.value).toBe(5);
+        expect(ll.head?.next).toBeNull();
+    });
+
+    it('Should initialize with node', () => {
+        const node = new LinkedListNode<number>(5);
+        node.next = new LinkedListNode<number>(3);
+        const ll = new LinkedList<number>(node);
+        expect(ll.head?.value).toBe(5);
+        expect(ll.head?.next?.value).toBe(3);
+        expect(ll.head?.next?.next).toBeNull();
+    });
+
+    it('Should initialize with array of values', () => {
+
+        const ll = new LinkedList<number>([1, 2, 3]);
+        expect(ll.head?.value).toBe(1);
+        expect(ll.head?.next?.value).toBe(2);
+        expect(ll.head?.next?.next?.value).toBe(3);
+        expect(ll.head?.next?.next?.next).toBe(null);
+    });
+
+    it('Should iterate over node values in list', () => {
+        const ll = new LinkedList<number>([1, 2, 3, 4, 5]);
+        expect([...ll]).toEqual([1, 2, 3, 4, 5]);
+    });
+    it('Should append nodes to the list', () => {
+        const ll = new LinkedList<number>();
+        ll.append(1);
+        expect(ll.head?.value).toBe(1);
+        ll.append(2);
+        expect(ll.head?.next?.value).toBe(2);
+
+    });
+    it('Should prepend nodes to the list', () => {
+        const ll = new LinkedList<number>();
+        ll.prepend(1);
+        expect(ll.head?.value).toBe(1);
+        ll.prepend(2);
+        expect(ll.head?.value).toBe(2);
+        expect(ll.head?.next?.value).toBe(1);
+
+    });
+
+    it('Should remove node from the list', () => {
+        const ll = new LinkedList<number>([1, 2, 3, 4]);
+        ll.remove(ll.head as LinkedListNode<number>);
+        expect(ll.head?.value).toBe(2);
+        ll.remove(ll.head?.next as LinkedListNode<number>);
+        expect(ll.head?.next?.value).toBe(4);
+        expect(ll.head?.value).toBe(2);
+
+    });
+    it('Should find a node from the list', () => {
+        const ll = new LinkedList<number>([1, 2, 3, 4]);
+        const node = ll.find(n => n.value === 3);
+        expect(node).toEqual(ll.head?.next?.next);
+        const nothing = ll.find(n => n.value === 10);
+        expect(nothing).toBeUndefined();
+    });
+});
