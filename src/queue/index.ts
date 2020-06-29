0a0e1284cbc94b05f085adfd19fc41c108cc121d
Ben Turner <bbgrabbag@gmail.com>
2020-06-29 10:53:49 -0600

    added QueueStack

--- src/queue/index.ts
+++ src/queue/index.ts
@@ -1,3 +1,5 @@
+import { Stack } from '../stack';
+
 class QueueNode<D>{
     private _data: D;
     private _next: QueueNode<D> | null = null;
@@ -21,7 +23,7 @@ class QueueNode<D>{
 export class Queue<D> {
     private _head: QueueNode<D> | null = null;
     private _last: QueueNode<D> | null = null;
-    private _length = 0;
+    protected _length = 0;
 
     public get length(): number {
         return this._length;
@@ -31,7 +33,7 @@ export class Queue<D> {
         return this._head?.data || null;
     }
 
-    add(data: D): void {
+    enqueue(data: D): void {
         const node = new QueueNode<D>(data);
         if (this._head === null || this._last === null) {
             this._head = node;
@@ -43,8 +45,8 @@ export class Queue<D> {
         this._length++;
     }
 
-    remove(): D | null {
-        if (this._head === null || this._last === null) return null;
+    dequeue(): D {
+        if (this._head === null || this._last === null) throw Error('Cannot dequeue from an empty queue');
         const node = this._head;
         if (node === this._last) this._last = null;
         this._head = this._head.next;
@@ -52,4 +54,45 @@ export class Queue<D> {
         return node.data;
     }
 
+}
+
+export class QueueStack<D> extends Queue<D>{
+
+    private _adder = new Stack<D>();
+    private _remover = new Stack<D>();
+    private _next: QueueNode<D> | null = null;
+
+    constructor() {
+        super();
+    }
+
+    private _reverse(from: Stack<D>, to: Stack<D>): void {
+        while (from.length) to.push(from.pop());
+    }
+
+    peek(): D | null {
+        return this._next?.data || null;
+    }
+
+
+    enqueue(data: D): void {
+        if (this._next === null) this._next = new QueueNode<D>(data);
+        if (this._remover.length) {
+            this._reverse(this._remover, this._adder);
+        }
+        this._adder.push(data);
+        this._length++;
+    }
+
+    dequeue(): D {
+        if (!this._remover.length) {
+            if (!this._adder.length) throw Error('Cannot dequeue from an empty queue');
+            this._reverse(this._adder, this._remover);
+        }
+        const output = this._remover.pop();
+        const next = this._remover.peek();
+        this._next = next === null ? null : new QueueNode<D>(next);
+        this._length--;
+        return output;
+    }
 }
--- src/queue/queue.spec.ts
+++ src/queue/queue.spec.ts
@@ -1,20 +1,20 @@
-import { Queue } from '.';
+import { Queue, QueueStack } from '.';
 
-describe('Queue data structure', () => {
+xdescribe('Queue data structure', () => {
 
     it('Should initialize as empty', () => {
         const queue = new Queue();
         expect(queue.peek()).toBe(null);
-        expect(queue.remove()).toBe(null);
         expect(queue.length).toBe(0);
+        expect(() => queue.dequeue()).toThrowError('Cannot dequeue from an empty queue');
     });
 
     it('Should add items to the end', () => {
         const queue = new Queue();
-        queue.add(1);
+        queue.enqueue(1);
         expect(queue.length).toBe(1);
         expect(queue.peek()).toBe(1);
-        queue.add(2);
+        queue.enqueue(2);
         expect(queue.length).toBe(2);
         expect(queue.peek()).toBe(1);
     });
@@ -23,12 +23,77 @@ describe('Queue data structure', () => {
         const queue = new Queue<number>();
         let i = 0;
         const length = 5;
-        while (++i < length) queue.add(i);
+        while (++i < length) queue.enqueue(i);
         let j = 0;
         while (++j < length) {
-            expect(queue.remove()).toBe(j);
+            expect(queue.dequeue()).toBe(j);
             expect(queue.length).toBe(length - j - 1);
         }
         expect(queue.peek()).toBe(null);
+        expect(() => queue.dequeue()).toThrowError('Cannot dequeue from an empty queue');
+
+    });
+});
+
+describe('QueueStack data structure', () => {
+    it('Should initialize empty', () => {
+        const queue = new QueueStack<number>();
+        expect(queue.length).toBe(0);
+        expect(queue.peek()).toBe(null);
+    });
+
+    it('Should add items', () => {
+        const queue = new QueueStack<number>();
+        let i = 0;
+        while (++i < 10) {
+            queue.enqueue(i);
+            expect(queue.length).toBe(i);
+            expect(queue.peek()).toBe(1);
+        }
+    });
+
+    it('Should remove items', () => {
+        const queue = new QueueStack<number>();
+        let i = 0;
+        while (++i < 5) {
+            queue.enqueue(i);
+        }
+        i = 0;
+        while (++i < 4) {
+            const value = queue.dequeue();
+            expect(value).toBe(i);
+            expect(queue.length).toBe(4 - i);
+            expect(queue.peek()).toBe(i + 1);
+        }
+        const value = queue.dequeue();
+        expect(value).toBe(4);
+        expect(queue.peek()).toBe(null);
+        expect(queue.length).toBe(0);
+        expect(() => queue.dequeue()).toThrowError('Cannot dequeue from an empty queue');
+
+
+    });
+
+    it('Should add and remove items', () => {
+        const queue = new QueueStack<number>();
+        queue.enqueue(1);
+        queue.enqueue(2);
+        queue.enqueue(3);
+        expect(queue.length).toBe(3);
+        queue.dequeue();
+        expect(queue.peek()).toBe(2);
+        expect(queue.length).toBe(2);
+        queue.enqueue(4);
+        expect(queue.length).toBe(3);
+        expect(queue.peek()).toBe(2);
+        queue.dequeue();
+        queue.dequeue();
+        expect(queue.length).toBe(1);
+        expect(queue.peek()).toBe(4);
+        queue.dequeue();
+        expect(queue.length).toBe(0);
+        expect(queue.peek()).toBe(null);
+
+
     });
 });
